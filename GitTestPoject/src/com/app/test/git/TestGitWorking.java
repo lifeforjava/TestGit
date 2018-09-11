6c6ed670894a2e6b80720abea850a746e0de0e9b
lifeforjava <lifeforjava786@gmail.com>
2018-09-11 11:53:31 +0530

    first commit

---
+++ GitTestPoject/src/com/app/test/git/TestGitWorking.java
@@ -0,0 +1,12 @@
+package com.app.test.git;
+
+//Demo for understanding self
+//its not related to the other user 
+public class TestGitWorking {
+
+	public static void main(String[] args) {
+		System.out.println("test Basic working of git");
+
+	}
+
+}
