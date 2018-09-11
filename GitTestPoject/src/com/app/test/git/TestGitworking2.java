d62cfec07b721be69976aae572053476427ce93d
lifeforjava <lifeforjava786@gmail.com>
2018-09-11 13:29:45 +0530

    commited from the eclipse

---
+++ GitTestPoject/src/com/app/test/git/TestGitworking2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.app.test.git;
+
+/**
+ * @author SUSH
+ *
+ */
+public class TestGitworking2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("dont waste u r time here this is for my practice and checking");
+
+	}
+
+}
